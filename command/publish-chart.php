4ea0ea1b6cae2e120bfd96ed576392aeeaefedb2
Laurent Guigo <laurent.guigo@gmail.com>
2014-12-03 09:06:16 +0100

    publish chart command line

--- command/publish-chart.php
+++ command/publish-chart.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * Created by Laurent GUIGO
- * Date: 01/12/14
- * Time: 17:56
- */
 
 /*
  * This scripts triggers the chart publication process.
@@ -17,6 +12,7 @@ date_default_timezone_set('Europe/Berlin');
 
 require_once ROOT_PATH . 'lib/bootstrap.php';
 require_once ROOT_PATH . 'lib/utils/chart_publish.php';
+
 
 $chart_id = $argv[1];
 $chart = ChartQuery::create()->findPK($chart_id);
